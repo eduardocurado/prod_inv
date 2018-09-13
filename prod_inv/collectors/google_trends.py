94814a42b3b577b1e43619c07cdaa132cadd6001
educurado <eduardo.curado@adianta.com.br>
2018-09-13 19:24:21 -0300

    Test google trends

--- prod_inv/collectors/google_trends.py
+++ prod_inv/collectors/google_trends.py
@@ -20,12 +20,17 @@ def get_trends(base_date, end_date):
     i = 0
 
     while i <= len(all_tickers) + 1:
-        trends = pytrends.get_historical_interest(kw_list[i:i+5], year_start=int(date_window[:4]), month_start=int(date_window[5:7]),
-                                         day_start=int(date_window[8:10]), hour_start=int(date_window[11:13]),
-                                         year_end=int(date_end[:4]),
-                                         month_end=int(date_end[5:7]), day_end=int(date_end[8:10]), hour_end=int(date_end[11:13]),
-                                         cat=0, geo='', gprop='', sleep=60)
-
+        trends = pytrends.get_historical_interest(kw_list[i:i + 5], year_start=int(date_window[:4]),
+                                                  month_start=int(date_window[5:7]),
+                                                  day_start=int(date_window[8:10]),
+                                                  hour_start=int(date_window[11:13]),
+                                                  year_end=int(date_end[:4]),
+                                                  month_end=int(date_end[5:7]),
+                                                  day_end=int(date_end[8:10]),
+                                                  hour_end=int(date_end[11:13]),
+                                                  cat=0, geo='', gprop='', sleep=60)
+        import ipdb;
+        ipdb.set_trace()
         trends = trends.drop(['isPartial'], axis=1).reset_index().copy()
         columns_trends = trends.columns
         for index, row in trends.iterrows():
@@ -36,12 +41,11 @@ def get_trends(base_date, end_date):
                 try:
                     google_entity = GoogleTrends(date, col, row[col])
                     db.session.add(google_entity)
-
+                    db.session.commit()
                 except Exception:
                     db.session.rollback()
-                    print('Already has value ' + col + str(date))
+                    print('Already has value ' + col + ' ' + str(date))
 
-        db.session.commit()
         i += 5
 
     return 'Success'
