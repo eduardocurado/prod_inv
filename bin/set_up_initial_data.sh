002acd5290113a963023f0d585f8a1ddd9ef8b2c
Eduardo Curado <dubis31@gmail.com>
2018-09-27 02:12:25 -0300

    minor fix

--- bin/set_up_initial_data.sh
+++ bin/set_up_initial_data.sh
@@ -7,7 +7,7 @@ echo Got Historical Data 86400!
 curl 'http://localhost:5000/get_historical_tickers?period=14400&historical=1'
 echo Got Historical Data 14400!
 
-curl 'http://localhost:5000/get_historical_google_trends?period=14400&historical=20'
+curl 'http://localhost:5000/get_historical_google_trends?period=14400&historical=10'
 echo Got Google Trends
 
 curl 'http://localhost:5000/get_historical_tas?period=86400&historical=1'
