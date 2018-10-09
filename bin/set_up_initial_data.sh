be80323e26df83926ce8c375a67f2903882bff73
Eduardo Curado <dubis31@gmail.com>
2018-10-09 00:30:49 -0300

    fixing google trends

--- bin/set_up_initial_data.sh
+++ bin/set_up_initial_data.sh
@@ -8,7 +8,7 @@ curl 'http://localhost:5000/get_historical_tickers?period=14400&historical=8'
 echo Got Historical Data 14400!
 
 
-curl 'http://localhost:5000/get_historical_google_trends?period=14400&historical=10'
+curl 'http://localhost:5000/get_historical_google_trends?period=14400&historical=40'
 echo Got Google Trends
 
 curl 'http://localhost:5000/get_historical_tas?period=86400&historical=8'
